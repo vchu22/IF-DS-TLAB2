15f49d17f5f8d1943ef0d23f0da4a1f36bdcd6d1
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-04-03 19:39:29 -0400

    finished make_plot in visualize.py

--- visualize.py
+++ visualize.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 
+def make_plot(sentiments: list) -> None:
+    """
+    Plot a graph for the counts we get for each sentiment.
 
-def make_plot(sentiments: list) -> list:
+    Args:
+        sentiments (list): a list containing the sentiment strings for each review comment.
     """
-    INSERT DOCSTRING HERE
-    """
-    pass
+    sentiment_counts = {}
+    for s in sentiments:
+        sentiment_counts[s] = sentiment_counts.get(s, 0) + 1
+    plt.xlabel("Sentiments")
+    plt.ylabel("Counts")
+    plt.bar(sentiment_counts.keys(), sentiment_counts.values(), color = 'blue')
+    plt.savefig("images/sentiments-1.png")
+    plt.close()
