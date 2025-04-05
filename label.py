b7064beeade7fa5ebce203a80237d7f74a0b909c
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-04-05 16:28:29 -0400

    finish main.py and edit system_prompt in label.py

--- label.py
+++ label.py
@@ -18,7 +18,6 @@ def get_sentiment(text: list) -> list:
         
     system_prompt = """
     You are tasked with reading a list of strings representing the customers' reviews for a product and produce a list of sentiments with only three possible values: "negative", "neutral", "positive", and "irrelevant". For example, given the input list below:
-    ```
     [
     "this ring smells weird, don't recomend",
     "I love this ring, I use it all the time when working out.",
@@ -27,11 +26,9 @@ def get_sentiment(text: list) -> list:
     "its a ring",
     "Bought this ring and it came broken. rip-off."
     ]
-    ```
+
     the output should be:
-    ```
     ["negative", "positive", "positive", "neutral", "irrelevant", "negative"]
-    ```
     """
 
     prompt = f"""
--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from visualize import make_plot
 
 import json
 
-
 def run(filepath: str):
     """
-    INSERT DOCSTRING HERE
+    Run a comprehensive pipeline that gets reviews from a local file, get a list for sentiments, and plot a barchart that represents the sentiment ratio.
+
+    Args:
+        filepath (string): the path to the json file containing the review texts.
     """
     # open the json object
-    ...
+    with open(filepath) as f:
+        d = json.load(f)
 
     # extract the reviews from the json file
-    ...
+    reviews = d["results"]
 
     # get a list of sentiments for each line using get_sentiment
-    ...
+    sentiments = get_sentiment(reviews)
 
     # plot a visualization expressing sentiment ratio
-    ...
+    make_plot(sentiments)
 
     # return sentiments
-    ...
+    return sentiments
 
 
 if __name__ == "__main__":
